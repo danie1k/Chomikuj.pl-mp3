4e2ad01de42bf39de3867a3f9d453fbbedcead52
Daniel Kuruc <daniel@kuruc.pl>
2019-05-09 23:08:55 +0200

    Add Chrome version, 2019.02

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Chomikuj.pl mp3 downloader
-A Firefox browser add-on.
+A Firefox & Chrome browser add-on.
 
 Download mp3's from Chomikuj.pl without logging in!
--- background.js
+++ background.js
@@ -1,25 +1,45 @@
-browser.menus.create({
-    id: "chomik-download-selection",
-    title: "Download selected mp3's",
-    contexts: ["selection"]
-});
-
-browser.menus.create({
-    id: "chomik-download-all",
-    title: "Download all mp3's on page",
-    contexts: ["all"]
-});
+var browser = !!browser ? browser : chrome;
+var contextMenus = !!browser.menus ? browser.menus : browser.contextMenus;
 
 
-browser.menus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === "chomik-download-selection" || info.menuItemId === "chomik-download-all") {
+var downloadResults = function (results) {
+    var filesToDownload = results[0];
+    Object.keys(filesToDownload).forEach(function (id) {
+        browser.downloads.download({
+            url: filesToDownload[id].url,
+            // There is a bug in Chromium causing `filename` to be ignored
+            // https://bugs.chromium.org/p/chromium/issues/detail?id=579563
+            filename: filesToDownload[id].filename,
+            conflictAction: "overwrite",
+            saveAs: true
+        });
+    });
+};
+
+
+var menuAction = function (info, tab) {
+    // Chrome
+    if (browser.hasOwnProperty('contextMenus')) {
+        browser.tabs
+            .executeScript(tab.id, { file: "chomikuj.js" }, downloadResults);
+    }
+    // Firefox
+    else {
         browser.tabs
             .executeScript(tab.id, { file: "chomikuj.js" })
-            .then(function (results) {
-                var filesToDownload = results[0];
-                Object.keys(filesToDownload).forEach(function (id) {
-                    browser.downloads.download(filesToDownload[id]);
-                });
-            });
+            .then(downloadResults);
     }
+};
+
+
+contextMenus.create({
+    title: "Download selected mp3's",
+    contexts: ["selection"],
+    onclick: menuAction
+});
+
+contextMenus.create({
+    title: "Download all mp3's on page",
+    contexts: ["all"],
+    onclick: menuAction
 });
